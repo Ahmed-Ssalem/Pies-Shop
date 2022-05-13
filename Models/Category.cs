d40d721acfadff17e9e1d2451e35bd4a3399f59c
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-05-13 22:32:41 +0200

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
+
+        //public Category(int id, string name, string description)
+        //{
+        //    CategoryId = id;
+        //    CategoryName = name;
+        //    Description = description;
+        //}
+    }
+
+    
+}
---
+++ Models/PieRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _appDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _appDbContext.Pies.Include(c => c.Category).Where(c => c.IsPieOfTheWeek);
+
+            }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+                return _appDbContext.Pies.FirstOrDefault(c => c.PieId == pieId);
+
+        }
+    }
+}
